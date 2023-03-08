e7a5e023612ee8f1ef2d42f9842cf63e3e4831d0
nsieverywhere <nsikansimon@gmail.com>
2023-03-08 23:43:36 +0100

    mongodb update

--- utils/connectdb.js
+++ utils/connectdb.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
-const connectMongo = async () => mongoose.connect(process.env.MONGO_URI);
+const connectMongo = async () =>
+  mongoose.connect(
+    "mongodb+srv://admin-nsi:q6CNybJ1n4fWjv66@cluster0.kbourxq.mongodb.net/?retryWrites=true&w=majority"
+  );
 
 export default connectMongo;
+
+// process.env.MONGO_URI;
