573c322f21778e4018b426d44831e5415f76a67a
Nsikan Simon <nsikansimon@gmail.com>
2023-05-12 15:16:10 +0100

    Update connectdb.js

--- utils/connectdb.js
+++ utils/connectdb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connectMongo = async () => mongoose.connect("mongodb+srv://admin-nsi:a5enLo0RGTW0TKW8@cluster0.kbourxq.mongodb.net/?retryWrites=true&w=majority");
+const connectMongo = async () => mongoose.connect("mongodb://127.0.0.1/Howfar");
 
 export default connectMongo;
 
